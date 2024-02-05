e50692583c5bc160a593359636ff1cccf1259f3e
Abdo-Walid12358 <134791898+Abdo-Walid12358@users.noreply.github.com>
2024-02-05 13:52:30 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ const resultElement = document.querySelector(".result");
 options.forEach(btn => {
     btn.addEventListener("click", (e) => {
         try{
-            const num1 = input1.value;
-            const num2 = input2.value;
-    
-            const mark = e.target.getAttribute("data-mark");
-    
-            let values = `${num1} ${mark} ${num2}`;
-    
-            let result = eval(values);
-    
-            input1.value = '';
-            input2.value = '';
+            const num1 = parseFloat(input1.value);
+            const num2 = parseFloat(input2.value);
 
-            if(!isNaN(result)){
-                resultElement.innerHTML = result;
+            if(typeof num1 == "number" && typeof num2 == "number"){
+                const mark = e.target.getAttribute("data-mark");
+                
+                let result = getResult(num1, mark, num2);
+        
+                input1.value = '';
+                input2.value = '';
+    
+                if(!isNaN(result)){
+                    resultElement.innerHTML = result;
+                }else{
+                    alert("ERROR");
+                }
             }else{
                 alert("ERROR");
             }
@@ -31,3 +33,19 @@ options.forEach(btn => {
         }
     })
 });
+
+function getResult(num1, mark, num2){
+    let res;
+
+    if(mark == "+"){
+        res = num1 + num2;
+    }else if(mark == "-"){
+        res = num1 - num2;
+    }else if(mark == "*"){
+        res = num1 * num2;
+    }else if(mark == "/"){
+        res = num1 / num2;
+    }
+
+    return res;
+}
